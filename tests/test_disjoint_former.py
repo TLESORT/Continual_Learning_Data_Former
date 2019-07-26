b14bdf95eec9f4397fb034e9b2088bcf3eee5ae9
TLESORT <t.lesort@gmail.com>
2019-07-26 09:47:40 +0200

    some remaining assert removed

--- tests/test_disjoint_former.py
+++ tests/test_disjoint_former.py
@@ -1,4 +1,3 @@
-
 import pytest
 from ..Sequence_Formers.disjoint import Disjoint
 import os
@@ -19,18 +18,22 @@ class args(object):
         self.path_only = None
 
     def set_paths(self):
-        assert self.dir is not None
-        assert self.dataset is not None
-        self.o = os.path.join(self.dir,'Data','Tasks',self.dataset)
-        self.i = os.path.join(self.dir,'Data','Datasets')
 
+        if self.dir is None:
+            raise AssertionError("Dir is None")
+        if self.dataset is None:
+            raise AssertionError("dataset is None")
+        self.o = os.path.join(self.dir, 'Data', 'Tasks', self.dataset)
+        self.i = os.path.join(self.dir, 'Data', 'Datasets')
 
     def add_supp_parameters(self, method):
         pass
 
+
 @pytest.fixture
 def get_args():
     return args()
+
 
 # #@pytest.mark.parametrize("datasets", ["mnist", "fashion", "kmnist","cifar10","LSUN","core50"])
 @pytest.mark.parametrize("dataset", ["MNIST", "fashion", "kmnist", "cifar10"])
@@ -57,15 +60,15 @@ def test_disjoint_vanilla(tmpdir, get_args, dataset, n_tasks):
     Data_Former = Disjoint(args)
     Data_Former.formating_data()
 
-    filename =  "disjoint_{}_{}.pt".format(n_tasks,"train")
+    filename = "disjoint_{}_{}.pt".format(n_tasks, "train")
 
-    if not os.path.isfile(os.path.join(tmpdir,"Data","Tasks",dataset,filename)):
+    if not os.path.isfile(os.path.join(tmpdir, "Data", "Tasks", dataset, filename)):
         raise AssertionError("Test fail with train file")
 
-    filename =  "disjoint_{}_{}.pt".format(n_tasks,"valid")
-    if not os.path.isfile(os.path.join(tmpdir,"Data","Tasks",dataset,filename)):
+    filename = "disjoint_{}_{}.pt".format(n_tasks, "valid")
+    if not os.path.isfile(os.path.join(tmpdir, "Data", "Tasks", dataset, filename)):
         raise AssertionError("Test fail with valid file")
 
-    filename =  "disjoint_{}_{}.pt".format(n_tasks,"test")
-    if not os.path.isfile(os.path.join(tmpdir,"Data","Tasks",dataset,filename)):
+    filename = "disjoint_{}_{}.pt".format(n_tasks, "test")
+    if not os.path.isfile(os.path.join(tmpdir, "Data", "Tasks", dataset, filename)):
         raise AssertionError("Test fail with test file")

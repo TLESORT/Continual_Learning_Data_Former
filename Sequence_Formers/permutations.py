6ebfd22a3746173089cf63ee72cf502c2cf8c781
TLESORT <t.lesort@gmail.com>
2020-03-19 08:40:34 +0100

    update permutation code and tests

--- Sequence_Formers/permutations.py
+++ Sequence_Formers/permutations.py
@@ -1,25 +1,44 @@
 import torch
+import os
 from Sequence_Formers.sequence_former import Sequence_Former
 from copy import deepcopy
+
+if os.path.exists("Sequence_Formers"):  # check if we are in the folder Continual_Learning_Data_Former
+    from data_utils import load_data, check_and_Download_data, get_images_format
+else:
+    from ..data_utils import get_images_format
+
 
 class Permutations(Sequence_Former):
     '''Scenario : In this scenario, for each tasks all classes are available, however for each task pixels are permutated.
     The goal is to test algorithms where all data for each classes are not available simultaneously and are available from
      different mode of th distribution (different permutation modes).'''
 
-    def __init__(self, args):
-        super(Permutations, self).__init__(args)
+    def __init__(self, path="./Data", dataset="MNIST", tasks_number=1, download=False, train=True):
+        self.num_pixels = 0  # will be set in prepare_formatting
+        self.perm_file = ""  # will be set in prepare_formatting
+        super(Permutations, self).__init__(path=path,
+                                           dataset=dataset,
+                                           tasks_number=tasks_number,
+                                           scenario="Rotations",
+                                           download=download,
+                                           train=train,
+                                           num_classes=10)
+
+    def prepare_formatting(self):
 
         self.list_perm = []
-
         self.num_pixels = self.imageSize * self.imageSize * self.img_channels
+        self.perm_file = os.path.join(self.o, '{}_{}_train.pt'.format("ind_permutations", self.tasks_number))
 
-        p = torch.FloatTensor(range(self.num_pixels)).long()
-        for _ in range(self.n_tasks):
-            self.list_perm.append(p)
-            p = torch.randperm(self.num_pixels).long().view(-1)
-
-        torch.save(self.list_perm, self.o_train.replace('permutations', 'ind_permutations'))
+        if os.path.isfile(self.perm_file):
+            self.list_perm = torch.load(self.perm_file)
+        else:
+            p = torch.FloatTensor(range(self.num_pixels)).long()
+            for _ in range(self.tasks_number):
+                self.list_perm.append(p)
+                p = torch.randperm(self.num_pixels).long().view(-1)
+            torch.save(self.list_perm, self.perm_file)
 
     def transformation(self, ind_task, data):
         p = self.list_perm[ind_task]
---
+++ tests/test_permutations.py
@@ -0,0 +1,18 @@
+import pytest
+from tests.utils_tests import check_task_sequences_files
+from Sequence_Formers.permutations import Permutations
+import os
+
+dataset_size = 100
+dir_data = "./Archives"
+
+
+# #@pytest.mark.parametrize("datasets", ["mnist", "fashion", "kmnist","cifar10","LSUN","core50"])
+@pytest.mark.slow
+@pytest.mark.parametrize("dataset", ["MNIST"])
+@pytest.mark.parametrize("n_tasks", [3, 5])
+def test_permutations_train(dataset, n_tasks):
+    # no need to download the dataset again for this test (if it already exists)
+    input_folder = os.path.join(dir_data, 'Data')
+    Data_Former = Permutations(path=input_folder, dataset=dataset, tasks_number=n_tasks, download=False, train=True)
+    check_task_sequences_files(scenario="Rotations", folder=dir_data, n_tasks=n_tasks, dataset=dataset, train=True)
